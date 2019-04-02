b762ac28b1c8bcc3598804379327cddffba22ea8
MaiaRojas <maia.rt.46@gmail.com>
2019-04-02 12:28:05 -0500

    wip

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const XLSX = require('xlsx');
 require('dotenv').config();
 
 firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID
+  apiKey: 'AIzaSyAXbaEbpq8NOfn0r8mIrcoHvoGRkJThwdc',
+  authDomain: 'laboratoria-la.firebaseapp.com',
+  projectId: 'laboratoria-la'
 });
 
 const workbook = XLSX.readFile(process.env.FILE_PATH);
@@ -19,7 +19,9 @@ const getUserData = (index) => ({
     email: sheet[`B${index}`].v,
     password: sheet[`C${index}`].v,
     profile: {
+        email: sheet[`B${index}`].v,
         name: sheet[`A${index}`].v,
+        // github: sheet[`D${index}`].v,
     },
     cohorts: {
         [process.env.DEFAULT_COHORT]: process.env.DEFAULT_ROLE,
@@ -32,7 +34,7 @@ const sendRequest = (data, token) => {
         method: 'PUT',
         body: JSON.stringify(data),
         headers: {
-            'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjBmNTVkZWZlOWU5YzU2ZmRhZTRkOGY0MDFjZjQ5Njc4YzE2N2MzYWEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGFib3JhdG9yaWEtbGEiLCJhdWQiOiJsYWJvcmF0b3JpYS1sYSIsImF1dGhfdGltZSI6MTUzNjM1NzE1NywidXNlcl9pZCI6IktZM25LOUVHN3VnVEs0Yk0yZWxESFhHRUJzajEiLCJzdWIiOiJLWTNuSzlFRzd1Z1RLNGJNMmVsREhYR0VCc2oxIiwiaWF0IjoxNTM2MzU3MTU3LCJleHAiOjE1MzYzNjA3NTcsImVtYWlsIjoiY2hhcXVpQGxhYm9yYXRvcmlhLmxhIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiY2hhcXVpQGxhYm9yYXRvcmlhLmxhIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.HLcYH027GCqKQkV8bXU1qw0XeLmRdq5aFWVAyTrHU6lu1dGyZXdMmwJaAd9oWC4b2M-F2dlwj0euLeLpLSYV_mZX60qUwnIgvJTsnyTDg-UmwEolAOPCXci_7AxShtMoj8yu7YfWQNvVx354tdy04tTiTNVbfCIRgIk2xR_s1oHRg8RU_XzlBhZrOyvKwi7b8NCdw7RnzW02m7JEu3UtAH8OPzfWttAyF-xxYWrN7Is2miQiKe8xvx3nXu5XpKeD058R1QsT2uN6IfBdvkgCg-Qnly9taQrusv1JFtkqm9yXc2rSiVQCbPRnbjR5CZrI6HXcmTdGQi-M4jJJNJ795g`,
+            'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImZmMWRmNWExNWI1Y2Y1ODJiNjFhMjEzODVjMGNmYWVkZmRiNmE3NDgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGFib3JhdG9yaWEtbGEiLCJhdWQiOiJsYWJvcmF0b3JpYS1sYSIsImF1dGhfdGltZSI6MTU1NDIyNDI0OSwidXNlcl9pZCI6IktZM25LOUVHN3VnVEs0Yk0yZWxESFhHRUJzajEiLCJzdWIiOiJLWTNuSzlFRzd1Z1RLNGJNMmVsREhYR0VCc2oxIiwiaWF0IjoxNTU0MjI0MjQ5LCJleHAiOjE1NTQyMjc4NDksImVtYWlsIjoiY2hhcXVpQGxhYm9yYXRvcmlhLmxhIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiY2hhcXVpQGxhYm9yYXRvcmlhLmxhIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.VDi6FpE-CDxfLk18vaSZC-BhocWrESJcn3guIgT08o5UQTy59AumCDir1JmXiAZ9kiYtvN4v3AIUPWR5YgPuJYAEab01lUXFJOi3E4z9HVfzBQV3CP257Cg0OB-fn4FIGjhHkSbKUgvuugXoAnKk20sF_P8Nt-FrMUd4rpDuK1yLEYReQQrd9IHTS2-hxWRD7Frv8vJ32vXcYO2krQ1HMd8nCKPlIliSOrooHFXDLCyWVdjMiu0KIVfw0mF9gDI-3rqR-JWsKJmxA9NYQSOVBmyOb4GYDftRDI3DBN3v11BJEL7er7sLSuC0K8qkhfEgZkYc6TpKtxbKKGPd9fCqHA`,
             'Content-Type': 'application/json',
         },
     });
@@ -53,7 +55,7 @@ firebase.auth().signInWithEmailAndPassword('chaqui@laboratoria.la', 'foobar42'
             console.log('index', index)
             if (sheet[`A${index + 2}`]) {
                 const data = getUserData(index + 2);
-                console.log(data)
+                // console.log(data)
                 updates.push(
                     sendRequest(data, token)
                         .then(response =>
